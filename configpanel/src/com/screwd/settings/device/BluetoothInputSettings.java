DirtyUnicorns/android_device_oppo_common

Change package name for uniformity
With Android O we want to stick to just one package name for device specific
gestures.

- Enable device extras

Change-Id: I8869908365bd9e0caa717d5ace388208e077d1a5

configpanel/src/com/dirtyunicorns/settings/device/BluetoothInputSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -41,7 +41,7 @@ import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
-import com.cyanogenmod.settings.device.utils.Constants;
+import com.dirtyunicorns.settings.device.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;

configpanel/src/com/dirtyunicorns/settings/device/ConfigPanelSearchIndexablesProvider.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
 
 import android.database.Cursor;
@@ -83,7 +83,7 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
         ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
         ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
         ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
-        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.cyanogenmod.settings.device";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.dirtyunicorns.settings.device";
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
         return ref;
     }

configpanel/src/com/dirtyunicorns/settings/device/GesturePadSettings.java
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
-import com.cyanogenmod.settings.device.utils.Constants;
-import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+import com.dirtyunicorns.settings.device.utils.Constants;
+import com.dirtyunicorns.settings.device.utils.NodePreferenceActivity;
 
 import android.os.Bundle;
 import android.os.IBinder;

configpanel/src/com/dirtyunicorns/settings/device/OclickService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -45,7 +45,7 @@ import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 
-import com.cyanogenmod.settings.device.utils.Constants;
+import com.dirtyunicorns.settings.device.utils.Constants;
 
 import java.util.UUID;
 
@@ -282,7 +282,7 @@ public class OclickService extends Service implements
                         }
                     }
 
-                    Intent keyIntent = new Intent("com.cyanogenmod.device.oppo.ACTION_OCLICK_KEY");
+                    Intent keyIntent = new Intent("com.dirtyunicorns.device.oppo.ACTION_OCLICK_KEY");
                     keyIntent.putExtra("key", key);
                     keyIntent.putExtra("action", action);
                     sendBroadcast(keyIntent);

configpanel/src/com/dirtyunicorns/settings/device/Startup.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
@@ -42,8 +42,8 @@ import android.view.KeyEvent;
 
 import java.io.File;
 
-import com.cyanogenmod.settings.device.utils.Constants;
-import com.cyanogenmod.settings.device.utils.FileUtils;
+import com.dirtyunicorns.settings.device.utils.Constants;
+import com.dirtyunicorns.settings.device.utils.FileUtils;
 
 public class Startup extends BroadcastReceiver {
 
@@ -140,7 +140,7 @@ public class Startup extends BroadcastReceiver {
             if (hasOClick()) {
                 updateOClickServiceState(context);
             }
-        } else if (intent.getAction().equals("cyanogenmod.intent.action.GESTURE_CAMERA")) {
+        } else if (intent.getAction().equals("dirtyunicorns.intent.action.GESTURE_CAMERA")) {
             long now = SystemClock.uptimeMillis();
             sendInputEvent(new KeyEvent(now, now, KeyEvent.ACTION_DOWN,
                     KeyEvent.KEYCODE_CAMERA, 0, 0,
@@ -154,7 +154,7 @@ public class Startup extends BroadcastReceiver {
             boolean enable) {
         PendingIntent pendingIntent = null;
         if (enable) {
-            Intent doubleTapIntent = new Intent("cyanogenmod.intent.action.GESTURE_CAMERA", null);
+            Intent doubleTapIntent = new Intent("dirtyunicorns.intent.action.GESTURE_CAMERA", null);
             pendingIntent = PendingIntent.getBroadcastAsUser(
                     context, 0, doubleTapIntent, 0, UserHandle.CURRENT);
         }

configpanel/src/com/dirtyunicorns/settings/device/TouchscreenGestureSettings.java
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.dirtyunicorns.settings.device;
 
-import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+import com.dirtyunicorns.settings.device.utils.NodePreferenceActivity;
 
 import android.os.Bundle;
 import android.provider.Settings;

configpanel/src/com/dirtyunicorns/settings/device/utils/Constants.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device.utils;
+package com.dirtyunicorns.settings.device.utils;
 
 import java.util.HashMap;
 import java.util.Map;

configpanel/src/com/dirtyunicorns/settings/device/utils/FileUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device.utils;
+package com.dirtyunicorns.settings.device.utils;
 
 import android.util.Log;
 

ScrewdAOSP/device_oppo_common

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

configpanel/src/com/screwd/settings/device/BluetoothInputSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -41,7 +41,7 @@ import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
-import com.cyanogenmod.settings.device.utils.Constants;
+import com.screwd.settings.device.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;

configpanel/src/com/screwd/settings/device/ConfigPanelSearchIndexablesProvider.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
 
 import android.database.Cursor;
@@ -83,7 +83,7 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
         ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
         ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
         ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
-        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.cyanogenmod.settings.device";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.screwd.settings.device";
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
         return ref;
     }

configpanel/src/com/screwd/settings/device/GesturePadSettings.java
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
-import com.cyanogenmod.settings.device.utils.Constants;
-import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+import com.screwd.settings.device.utils.Constants;
+import com.screwd.settings.device.utils.NodePreferenceActivity;
 
 import android.os.Bundle;
 import android.os.IBinder;

configpanel/src/com/screwd/settings/device/OclickService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -45,7 +45,7 @@ import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 
-import com.cyanogenmod.settings.device.utils.Constants;
+import com.screwd.settings.device.utils.Constants;
 
 import java.util.UUID;
 
@@ -282,7 +282,7 @@ public class OclickService extends Service implements
                         }
                     }
 
-                    Intent keyIntent = new Intent("com.cyanogenmod.device.oppo.ACTION_OCLICK_KEY");
+                    Intent keyIntent = new Intent("com.screwd.device.oppo.ACTION_OCLICK_KEY");
                     keyIntent.putExtra("key", key);
                     keyIntent.putExtra("action", action);
                     sendBroadcast(keyIntent);

configpanel/src/com/screwd/settings/device/Startup.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
@@ -42,8 +42,8 @@ import android.view.KeyEvent;
 
 import java.io.File;
 
-import com.cyanogenmod.settings.device.utils.Constants;
-import com.cyanogenmod.settings.device.utils.FileUtils;
+import com.screwd.settings.device.utils.Constants;
+import com.screwd.settings.device.utils.FileUtils;
 
 public class Startup extends BroadcastReceiver {
 
@@ -140,7 +140,7 @@ public class Startup extends BroadcastReceiver {
             if (hasOClick()) {
                 updateOClickServiceState(context);
             }
-        } else if (intent.getAction().equals("cyanogenmod.intent.action.GESTURE_CAMERA")) {
+        } else if (intent.getAction().equals("screwd.intent.action.GESTURE_CAMERA")) {
             long now = SystemClock.uptimeMillis();
             sendInputEvent(new KeyEvent(now, now, KeyEvent.ACTION_DOWN,
                     KeyEvent.KEYCODE_CAMERA, 0, 0,
@@ -154,7 +154,7 @@ public class Startup extends BroadcastReceiver {
             boolean enable) {
         PendingIntent pendingIntent = null;
         if (enable) {
-            Intent doubleTapIntent = new Intent("cyanogenmod.intent.action.GESTURE_CAMERA", null);
+            Intent doubleTapIntent = new Intent("screwd.intent.action.GESTURE_CAMERA", null);
             pendingIntent = PendingIntent.getBroadcastAsUser(
                     context, 0, doubleTapIntent, 0, UserHandle.CURRENT);
         }

configpanel/src/com/screwd/settings/device/TouchscreenGestureSettings.java
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.screwd.settings.device;
 
-import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+import com.screwd.settings.device.utils.NodePreferenceActivity;
 
 import android.os.Bundle;
 import android.provider.Settings;

configpanel/src/com/screwd/settings/device/utils/Constants.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device.utils;
+package com.screwd.settings.device.utils;
 
 import java.util.HashMap;
 import java.util.Map;

configpanel/src/com/screwd/settings/device/utils/FileUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device.utils;
+package com.screwd.settings.device.utils;
 
 import android.util.Log;
 